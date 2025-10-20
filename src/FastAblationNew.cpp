c454622f96786a87857fba1d738296089e203708
blackcael <blackcael@gmail.com>
2025-10-20 12:02:07 -0600

    triange_wave_test

--- src/FastAblationNew.cpp
+++ src/FastAblationNew.cpp
@@ -57,13 +57,35 @@ void CreateOutputData(int numberOfSamplesPerChannel, int chanCount,double* buffe
 	}
 }
 
-void GenerateSquareWave(int numSamples, double* buffer) {
+void GenerateSquareWave(int numSamples, double* buffer, double frequency) {
 	for (int i = 0; i < numSamples; i++) {
-		if (i % 2 == 0) {
+		if (i % (int)(numSamples / frequency) == 0) {
 			buffer[i] = 5.0;
 		}
 		else {
 			buffer[i] = 0.0;
+		}
+	}
+}
+
+void GenerateTriangleWave(int numberOfSamplesPerSecond, double frequency, double* buffer)
+{
+	double min = 0.0;
+	double max = 5.0;
+
+	double amplitude =  max - min;
+	double numberOfSamplesPerPeriod = frequency / numberOfSamplesPerSecond;
+
+	for (int i=0; i<numberOfSamplesPerSecond; i++)
+	{
+		int phase_index = i % (int)numberOfSamplesPerPeriod;
+		if (phase_index < numberOfSamplesPerPeriod / 2)
+		{
+			buffer[i] = (double)(min + (phase_index * amplitude / (numberOfSamplesPerPeriod / 2)));
+		}
+		else
+		{
+			buffer[i] = (double)(max - ((phase_index - numberOfSamplesPerPeriod / 2) * amplitude / (numberOfSamplesPerPeriod / 2)));
 		}
 	}
 }
@@ -88,13 +110,37 @@ void test() {
 	cout << "Total time taken: " << totalTime / (100 * 1000) << endl;
 }
 
+void triangleWaveTest(){
+	DAQControl daq = DAQControl("20BF9C2");
+	cout << "Starting triangle wave test..." << endl;
+	auto start = daq.getTimeinMicroseconds();
+	uint16_t num_iterations = 60000;
+	double rate = daq.getIdealRate(num_iterations);
+	cout << "Ideal rate according to DAQ: " << rate << " with num iterations: " << num_iterations << endl;
+	double buffer[(uint16_t)rate];
+	uint16_t totalTime = 0;
+
+	GenerateTriangleWave((uint16_t)rate, 375, buffer);
+	for (uint8_t i = 0; i < 100; i++) {
+		auto scanStart = daq.getTimeinMicroseconds();
+		daq.analogScanOut(0, vector<double>(buffer, buffer + (uint16_t)rate), true, rate);
+		auto scanEnd = daq.getTimeinMicroseconds();
+		totalTime += (scanEnd - scanStart);
+	}
+
+	cout << "Total time taken: " << totalTime / (100 * 1000) << endl;
+}
+
 
 int main()
 {
-	DataReciever r("10.10.10.10", 50007);
-	while (true) {
-		auto data = r.receiveData();
-		cout << "Received data with " << data.size() << " rows and " << data[0].size() << " columns." << endl;
-	}
+
+	// DataReciever r("10.10.10.10", 50007);
+	// while (true) {
+	// 	auto data = r.receiveData();
+	// 	cout << "Received data with " << data.size() << " rows and " << data[0].size() << " columns." << endl;
+	// }
+	triangleWaveTest();
+	return 0;
 
 }
